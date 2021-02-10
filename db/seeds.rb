690dbe31cd371710a71c3a16f920d26664b8df68
Natalia Gatti <gattinati.ng@gmail.com>
2021-02-09 22:35:20 -0300

    Rails Simple Airbnb.Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroing seeds"
+Flat.destroy_all
+
+puts "Creating 4 flats"
+
+4.times do
+  flats = Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Company.catch_phrase,
+    price_per_night: Faker::Commerce.price(range: 0..10.0, as_string: true).to_i,
+    number_of_guests: (1..6).to_a.sample.to_i,
+    photo: ["https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1495433324511-bf8e92934d90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1512916194211-3f2b7f5f7de3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"].sample
+  )
+end
+
+puts "4 flats created"
